4b5d6cbc3a070dd056e91c922f7b927b1428b926
quinwu <wukuan1995@outlook.com>
2017-07-16 22:43:05 +0800

    modify

--- Logistic-Regression/logistic_regression.py
+++ Logistic-Regression/logistic_regression.py
@@ -1,14 +1,23 @@
 import numpy as np
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 
 def sigmoid(z):
     return 1.0 / (1.0 + np.exp(-z))
 
-def costFunction(X,y,theta):
-    m = len(y.T)
+
+def costFunction(theta,X, y):
+    m = X.shape[0]
     J = 0
-    grad = np.zeros_like(theta)
     z = X.dot(theta)
-    J = (-y.dot(np.log(sigmoid(z))) - (1 - y).dot(np.log(1 - sigmoid(z)))) / m
-    grad = (X.T.dot((sigmoid(z) - y.T))/m)
-    return J ,grad 
+    J = (-y.T.dot(np.log(sigmoid(z))) - (1 - y.T).dot(np.log(1 - sigmoid(z)))) / m
+    return J[0]
+
+
+def gradient(theta,X, y): 
+    m = X.shape[0] #X.shape = (100,3)
+    grad = np.zeros_like(theta)
+    z = X.dot(theta.reshape(-1,1)) # theta.shape = (3,) theta.reshape(-1,1).shape = (3,1)
+    grad = (1.0 / m) * X.T.dot(( sigmoid(z) - y )) 
+    # grad =  X.T.dot(( sigmoid(z) - y )) 
+    return grad.flatten()
+
--- Logistic-Regression/main.py
+++ Logistic-Regression/main.py
@@ -1,34 +1,53 @@
 from logistic_regression import *
+import scipy.optimize as op
+import os
 
 def loadData():
-    file1 = open("ex2data1.txt", "r")
-    file2 = open("ex2data2.txt", "r")
-    data = np.loadtxt(file1,dtype=float,delimiter=',', unpack=True)
-    X = np.array(data[0:2,:])
-    y = np.array(data[2,:])
+    path  = os.getcwd() + '/ex2data1.txt'
+    data = np.loadtxt(path,dtype=float,delimiter=',')
+    X = data[:,0:2]
+    y = data[:,2]
     return X,y
 
-def plotScatter(X, y):
-    neg = (y == 0.0)
-    pos = (y != 0.0)
-    lable1 = plt.scatter(X[0,neg],X[1,neg],marker='o',c='b')
-    lable2 = plt.scatter(X[0,pos],X[1,pos],marker='o',c='r')
+def plotScatter():
+    X,y = loadData()
+    y = y.astype(int)
+    neg = (y == 0)
+    pos = (y == 1)
+    lable1 = plt.scatter(X[neg,0],X[neg,1],marker='o',c='b')
+    lable2 = plt.scatter(X[pos,0],X[pos,1],marker='o',c='r')
     plt.legend((lable1,lable2),('Admitted','Not admitted'))
     plt.xlabel('Exam 1 score')
     plt.ylabel('Exam 2 score')
     plt.show()
 
+def decisionBundary(theta):
+    X,y = loadData()
+    y = y.astype(int)
+    neg = (y == 0)
+    pos = (y == 1)
+    lable1 = plt.scatter(X[neg,0],X[neg,1],marker='o',c='b')
+    lable2 = plt.scatter(X[pos,0],X[pos,1],marker='o',c='r')
+    plt.legend((lable1,lable2),('Admitted','Not admitted'))
+    plt.xlabel('Exam 1 score')
+    plt.ylabel('Exam 2 score')
+    boundary_xs = np.array([np.min(X[:,1]), np.max(X[:,1])])
+    boundary_ys = (-1./theta[2])*(theta[0] + theta[1]*boundary_xs)
+    plt.plot(boundary_xs,boundary_ys,'b-',label='Decision Boundary')
+    plt.show()
+
+
 
 if __name__ == "__main__":
 
     X,y = loadData()
-    # plotScatter(X,y)
-    n = len(X)
-    m = len(X[0])  
-    X = np.c_[np.ones(m), X.T] # add one col
-    y = np.reshape(y,(1,m))
-
-    initial_theta = np.zeros((n+1,1))
-    cost,grad = costFunction(X,y,initial_theta)
-
-    
+    # plotScatter()
+    m,n = X.shape
+    X = np.c_[np.ones((m,1)), X]  # add one col
+    #y = np.reshape(y,(m,1))
+    y= np.c_[y]
+    initial_theta = np.zeros(X.shape[1])
+    res = op.minimize(costFunction, initial_theta, args=(X,y), method='TNC', jac=gradient, options={'maxiter':400})
+    theta = res.x
+    print theta
+    decisionBundary(theta)
